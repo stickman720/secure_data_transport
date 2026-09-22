b4a8c64e691fc24840095683344c71044e81df92
stickman720 <saeedpr720@gmail.com>
2026-09-22 10:43:36 +0330

    make design pattern structs for making responses cleaner .

--- api_schema/auth.go
+++ api_schema/auth.go
@@ -1,7 +1,14 @@
 package api_schema
 
+import (
+	sqlcpkg "secure_data_transport/sqlcpkg"
+)
 
 
+
+//====================================================================
+//requests
+//====================================================================
 
 type RegisterRequest struct {
 	Username string `json:"username"`
@@ -14,3 +21,22 @@ type VerifyEmailRequest struct {
 	Code string `json:"code"`
 }
 
+
+//====================================================================
+//responses
+//====================================================================
+
+type ErrorResponse struct {
+	Code string  `json:"code"`
+	Error string `json:"error"`
+}
+
+
+type RegisterResponse struct {
+	Message string `json:"message"`
+}
+
+
+type VerifyEmailResponse struct {
+	User sqlcpkg.User `json:"user"`
+}
